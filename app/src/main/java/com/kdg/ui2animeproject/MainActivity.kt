fe06480a931b3ff219e62d943e36e5f7df8be716
Jonas <jonas.nikolopoulos@student.kdg.be>
2024-05-02 11:50:24 +0200

    W3: Volgende al afgewerkt : Zet een horizontale scrollable list met de cars van een persoon onder het detail scherm van die persoon dat je in de vorige opdracht maakte. Zorg ervoor dat je lijst efficiënt werkt (lazy). Zorg ervoor dat je toepassing zowel in het engels als in het nederlands werkt (de data van personen en cars zijn wel eentalig) Als je je scherm in landscape modus toont, zal alle info niet meer zichtbaar zijn. Voorzie in dat geval een verticale scrollbar

--- app/src/main/java/com/kdg/ui2animeproject/MainActivity.kt
+++ app/src/main/java/com/kdg/ui2animeproject/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -20,19 +25,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.structuralEqualityPolicy
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kdg.ui2animeproject.model.countCharactersByAnimeSeriesId
 import com.kdg.ui2animeproject.model.getAnimeSeries
+import com.kdg.ui2animeproject.model.getCharacters
 import com.kdg.ui2animeproject.ui.theme.UI2AnimeProjectTheme
-import java.nio.channels.FileChannel.MapMode
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,44 +68,46 @@ fun DisplayAnimeSeries(modifier: Modifier = Modifier) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 20.dp, vertical = 30.dp),
+            .padding(horizontal = 20.dp, vertical = 30.dp)
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = currentAnime.title,
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
         Image(
             painter = painterResource(currentAnime.image),
             contentDescription = "Image of ${currentAnime.title}",
             contentScale = ContentScale.Crop,
             modifier = modifier
                 .fillMaxWidth()
-                .padding(bottom = 20.dp)
-                .heightIn(max = 500.dp)
-        )
-        Text(
-            text = "Title: ${currentAnime.title}",
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.padding(bottom = 16.dp)
+                .padding(bottom = 8.dp)
+                .heightIn(max = 475.dp)
         )
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             Text(
-                text = "Genre: ${currentAnime.genre}",
+                text = stringResource(id = R.string.anime_genre, currentAnime.genre)+ ": ${currentAnime.genre}",
                 style = MaterialTheme.typography.bodyMedium
             )
             Text(
-                text = "Rating: ${currentAnime.averageRating}",
+                text = stringResource(id = R.string.anime_rating,currentAnime.averageRating) +": ${currentAnime.averageRating}",
                 style = MaterialTheme.typography.bodyMedium
             )
         }
         Text(
-            text = "Number of Characters: ${countCharactersByAnimeSeriesId(currentAnime.id)}",
+            text = stringResource(id = R.string.anime_characterAmount)+": ${countCharactersByAnimeSeriesId(currentAnime.id)}",
             style = MaterialTheme.typography.bodySmall,
-            modifier = Modifier.padding(bottom = 16.dp)
+            modifier = Modifier.padding(bottom = 5.dp)
         )
 
-                    Row(
+        Row(
             modifier = Modifier
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.Center,
@@ -109,14 +117,57 @@ fun DisplayAnimeSeries(modifier: Modifier = Modifier) {
                 onClick = { currentIndex = (currentIndex - 1).coerceAtLeast(0) },
                 modifier = Modifier.padding(horizontal = 10.dp, vertical = 8.dp)
             ) {
-                Text(text = "Previous")
+                Text(text = stringResource(id= R.string.button_previous))
             }
             Spacer(modifier = Modifier.width(24.dp))
             Button(
-                onClick = { currentIndex = (currentIndex + 1).coerceAtMost(animeSeriesList.size - 1) },
+                onClick = {
+                    currentIndex = (currentIndex + 1).coerceAtMost(animeSeriesList.size - 1)
+                },
                 modifier = Modifier.padding(horizontal = 10.dp, vertical = 8.dp)
             ) {
-                Text(text = "Next")
+                Text(text = stringResource(id = R.string.button_next))
+            }
+        }
+        val characters = getCharacters().filter { it.animeSerieId == currentAnime.id }
+        LazyRow(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(40.dp),
+            contentPadding = PaddingValues(horizontal = 40.dp)
+        ) {
+            items(characters) { character ->
+                Column(
+                    modifier = Modifier
+                        .width(150.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = character.name,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Text(
+                            text = stringResource(id = R.string.character_powerlevel),
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                        Text(
+                            text = ": ${character.powerLevel}",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                    }
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Text(
+                            text = stringResource(id = R.string.character_specialability) + ": ",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                    }
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Text(
+                            text = character.specialAbility,
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                    }
+                }
             }
         }
     }
