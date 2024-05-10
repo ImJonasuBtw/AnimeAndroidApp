935618ce99a591683b991ef64eae36c75357074d
Jonas <jonas.nikolopoulos@student.kdg.be>
2024-05-10 14:39:27 +0200

    W4: Alle data naar dataSource verplaatst zoals oef

--- app/src/main/java/com/kdg/ui2animeproject/model/AnimeSeries.kt
+++ app/src/main/java/com/kdg/ui2animeproject/model/AnimeSeries.kt
@@ -1,6 +1,7 @@
 package com.kdg.ui2animeproject.model
 
 import androidx.annotation.DrawableRes
+import com.kdg.ui2animeproject.Data.AnimeDataSource.animeSeriesList
 import com.kdg.ui2animeproject.R
 
 data class AnimeSeries(
@@ -14,40 +15,6 @@ data class AnimeSeries(
     @DrawableRes val image: Int
 )
 
-val animeSeriesList = listOf(
-    AnimeSeries(
-        1,
-        "Naruto",
-        "2002-10-03",
-        "Action",
-        "Pierrot",
-        8.2,
-        true,
-        R.drawable.naruto
-    ),
-    AnimeSeries(
-        2,
-        "Attack on Titan",
-        "2013-04-07",
-        "Action",
-        "Wit Studio",
-        8.8,
-        true,
-        R.drawable.attackontitan
-    ),
-    AnimeSeries(
-        3,
-        "My Hero Academia",
-        "2016-04-03",
-        "Superhero",
-        "Bones",
-        8.5,
-        false,
-        R.drawable.bokuhero
-    ),
-
-
-    )
 
 fun getAnimeSeries(): Array
 <AnimeSeries> {
